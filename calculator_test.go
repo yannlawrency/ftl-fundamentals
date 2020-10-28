752805f365f0dbea6d259c1e862a878f7ed8733e
yann <yann.lawrence@gmail.com>
2020-10-28 09:36:20 +0000

    evolve test to use struct

--- calculator_test.go
+++ calculator_test.go
@@ -5,12 +5,25 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	a, b float64
+	want float64
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 0.1, b: 0.9, want: 1},
+		{a: 123, b: 321, want: 444},
+		{a: -10, b: 9, want: -1},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
 	}
 }
 
